eeac3aa423bbc519384e196311fa13c286d17053
Dibora Dawit <dawitdibora5106@gmail.com>
2024-10-30 10:45:43 +0300

    imported WelcomeMessage from ./components/WelcomeMessage

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello every one, I am learning React at ALX!"</h1>
+      <p>I am learning about JSX!</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
