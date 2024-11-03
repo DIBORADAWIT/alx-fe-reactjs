c3772c1eb35d881283a07ec11be63590bde9e3cf
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-03 16:07:40 +0200

    imortedUserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,6 +12,7 @@ import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
 import UserProfile from "./UserProfile";
 
+
 function App() {
   const [count, setCount] = useState(0);
 
