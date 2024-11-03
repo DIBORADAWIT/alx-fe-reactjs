7fe9e92ede71e9841efa7ab904f325b5f1fad21f
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-03 16:13:39 +0200

    added in to components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
-import UserProfile from "./UserProfile";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
