27fc421dd3a4cfdab0d2c6378f8e40d4f60c2528
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-07 05:38:42 +0200

    Removed the userData props passed through ProfilePage and UserInfo, as these will no longer be necessary

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -15,7 +15,7 @@ import UserContext from "./UserContext";
 function App() {
   const [count, setCount] = useState(0);
 
-  // const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
