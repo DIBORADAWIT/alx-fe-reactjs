9fe779f3240f7de2dd4b1cbcad433a7439fe0a54
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-07 05:34:51 +0200

    userData removed

--- alx-react-app-new/src/UserInfo.jsx
+++ alx-react-app-new/src/UserInfo.jsx
@@ -1,7 +1,16 @@
-import UserDetails from './UserDetails';
+import UserDetails from "./UserDetails";
 
 function UserInfo({ userData }) {
   return <UserDetails userData={userData} />;
 }
+
+const userData = useContext(UserContext);
+return (
+  <div>
+    <h2>User Info</h2>
+    <p>Name: {userData.name}</p>
+    <p>Email: {userData.email}</p>
+  </div>
+);
 
 export default UserInfo;
