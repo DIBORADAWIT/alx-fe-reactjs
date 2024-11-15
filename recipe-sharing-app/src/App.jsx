4cbc95e264694b778d89f1e2b53246dc83ae874c
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-15 08:25:56 +0200

    recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import useStore from "./useStore";
+function App() {
+  const [count, setCount] = useState(0);
+  const { increment, decrement, reset } = useStore();
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="App">
+        <h1>Recipe Sharing Application</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
