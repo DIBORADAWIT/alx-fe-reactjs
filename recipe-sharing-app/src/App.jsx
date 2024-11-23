cf765f4631dc5c0905ae97164df855812ffe76b0
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-23 20:43:09 +0200

    imported

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -9,6 +9,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import useStore from "./components/useStore";
 import RecipeDetails from "./components/RecipeDetails";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsLi";
 function App() {
   const [count, setCount] = useState(0);
   const { increment, decrement, reset } = useStore();
@@ -24,6 +26,8 @@ function App() {
           <Route path="/recipes" element={<RecipeDetails />} />
         </Routes>
       </div>
+      <FavoritesList />
+      <RecommendationsList />
     </>
   );
 }
