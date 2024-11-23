2cd6f685825ff75e7324df15586bff275f412681
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-23 20:31:58 +0200

    zustand update

--- recipe-sharing-app/src/components/useStore.js
+++ recipe-sharing-app/src/components/useStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 const useStore = create((set) => ({
   recipes: [],
+  favorites: [],
 
   addRecipe: (recipe) =>
     set((state) => ({
@@ -19,6 +20,21 @@ const useStore = create((set) => ({
         recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
       ),
     })),
+
+  addFavorite: (recipe) =>
+    set((state) => ({
+      favorites: [...state.favorites, recipe],
+    })),
+  removeFavorite: (id) =>
+    set((state) => ({
+      favorites: state.favorites.filter((recipe) => recipe.id !== id),
+    })),
+  getRecommendations: () => {
+    const { recipes, favorites } = get();
+    return recipes.filter((recipe) =>
+      favorites.some((fav) => fav.cuisine === recipe.cuisine)
+    );
+  },
 }));
 
 export default useStore;
