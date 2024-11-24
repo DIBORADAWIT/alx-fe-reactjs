13329c13e3369aaad2605379456a4de78b2de6e4
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-24 12:33:31 +0200

    all done

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Build a Todo App", completed: false },
   ]);
+
+  ["TodoList", "import TodoList"];
   return (
     <>
       <div>
