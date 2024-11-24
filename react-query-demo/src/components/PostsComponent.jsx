26ca0f4596b2b1981c02a24b325bf171442fbbe3
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-24 09:59:26 +0200

    posts

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+// Fetch function
+const fetchPosts = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return data;
+};
+
+const PostsComponent = () => {
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(["posts"], fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching posts: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
