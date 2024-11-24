04ac96d127daa7d59f270247f515bd05e5014bdb
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-24 11:19:18 +0200

    all done

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+      <hr />
+      {/* Render the nested route components here */}
+      <Outlet />
+    </div>
+  );
+};
+
+export default Profile;
