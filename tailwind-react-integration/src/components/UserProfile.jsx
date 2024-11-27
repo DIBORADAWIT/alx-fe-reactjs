e8ff5ae56a31c2ed2c096fb4a435e56534ad1795
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-27 13:00:43 +0200

    all done

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,9 +9,10 @@ function UserProfile() {
       ["md:p-8", "sm:p-4"]
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
-        ["sm:w-24", "sm:h-24", "md:w-36", "md:h-36"] ["text-lg", "md:text-xl",
-        "text-sm"] ["max-w-xs", "md:max-w-sm"] Developer at Example Co. Loves to
-        write code and explore new technologies.
+        ["hover:scale-110", "transition-transform", "duration-300",
+        "ease-in-out"] ["sm:w-24", "sm:h-24", "md:w-36", "md:h-36"] ["text-lg",
+        "md:text-xl", "text-sm"] ["max-w-xs", "md:max-w-sm"] Developer at
+        Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
