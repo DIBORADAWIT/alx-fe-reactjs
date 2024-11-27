bb9f895dfbe701576c5631c5ed559c92487ee497
Dibora Dawit <dawitdibora5106@gmail.com>
2024-11-27 12:52:39 +0200

    added all

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,8 +9,8 @@ function UserProfile() {
       ["md:p-8", "sm:p-4"]
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
+        ["max-w-xs", "md:max-w-sm"] Developer at Example Co. Loves to write code
+        and explore new technologies.
       </p>
     </div>
   );
