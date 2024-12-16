63bb5c19668cca40a5da6badea62f3f62e01d3a0
Dibora Dawit <dawitdibora5106@gmail.com>
2024-12-16 10:53:07 +0200

    all done

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,44 +1,35 @@
-import React, { useState } from "react";
-import "./App.css";
-import Search from "./components/Search";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [userData, setUserData] = useState(null);
-
-  const handleSearch = () => {
-    console.log("Searching for:", searchQuery);
-  };
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Enter GitHub username"
-      />
-      <button onClick={handleSearch}>Search</button>
-
+    <>
       <div>
-        {userData && (
-          <div>
-            <h2>{userData.name}</h2>
-            <p>{userData.bio}</p>
-            <a
-              href={userData.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View Profile
-            </a>
-            <Search />
-          </div>
-        )}
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
